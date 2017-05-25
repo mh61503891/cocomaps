613e926a93df91c746dbc180e5ad4a3026400a06
Masayuki Higashino <msyk@hgsn.info>
2017-05-26 03:44:21 +0900

    support Procfile

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+ruby '2.3.3'
+
+gem 'sinatra'
+gem 'puma'
+
+group :production do
+  gem 'rack-tracker'
+  gem 'rack-ssl-enforcer'
+end
+
+group :development do
+  gem 'foreman'
+  gem 'sinatra-contrib' # for sinatra/reloader
+end
---
+++ web.rb
@@ -0,0 +1,28 @@
+require 'sinatra/base'
+require 'csv'
+require 'digest/md5'
+
+module CoCoMaps
+  class Web < Sinatra::Base
+    set :public_folder, File.dirname(__FILE__) + '/www'
+
+    configure :production do
+      require 'rack/tracker'
+      use Rack::Tracker do
+        handler :google_analytics, tracker: ENV['GOOGLE_TRACKING_ID']
+      end
+      require 'rack/ssl-enforcer'
+      use Rack::SslEnforcer
+    end
+
+    configure :development do
+      set :bind, '0.0.0.0'
+      require 'sinatra/reloader'
+      register Sinatra::Reloader
+    end
+
+    get '/' do
+      send_file File.join(settings.public_folder, 'index.html')
+    end
+  end
+end
